05833d6e2d0f90c7973d340d66395980c9e87954
Luis Lopez <metfanforever@gmail.com>
2026-08-04 10:33:45 -0700

    mod bookapi

--- src/services/bookApi.js
+++ src/services/bookApi.js
@@ -1,1 +1,19 @@
+const BOOK_API_URL = "/.netlify/functions/random-books";
 
+export async function getRandomBook() {
+  const response = await fetch(BOOK_API_URL);
+
+  if (!response.ok) {
+    throw new Error("Unable to retrieve a book.");
+  }
+
+  const data = await response.json();
+
+  return {
+    title: data.title,
+    author: data.author,
+    firstPublishYear: data.firstPublishYear,
+    coverUrl: data.coverUrl,
+    bookUrl: data.bookUrl,
+  };
+}
