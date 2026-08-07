469ed88d235056128e676518e71ad9d865f0b3d1
Luis Lopez <metfanforever@gmail.com>
2026-08-07 14:58:56 -0700

    mod App.css Home add img skeleton-leaf + skeleton-leaf2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,26 +3,44 @@ import BooksPanel from "../components/BooksPanel";
 import InspirationalCarousel from "../components/InspirationalCarousel";
 import PictureFrame from "../components/PictureFrame";
 
+import skeletonLeaf from "../assets/images/skeleton-leaf.jpg";
+import skeletonLeafTwo from "../assets/images/skeleton-leaf2.jpg";
+
 import frameImageOne from "../assets/images/Journey.jpg";
 import frameImageTwo from "../assets/images/NewHope.jpg";
 
 function Home() {
   return (
-    <main className="container mt-5">
+    <main className="container mt-5 home-page">
+
+      <img
+        src={skeletonLeaf}
+        alt=""
+        aria-hidden="true"
+        className="home-leaf home-leaf-top-left"
+      />
+
+      <img
+        src={skeletonLeafTwo}
+        alt=""
+        aria-hidden="true"
+        className="home-leaf home-leaf-bottom-right"
+      />
+
       <div className="row align-items-start">
-      <div className="col-md-4">
-  <div className="page-reveal reveal-quote">
-    <QuotePanel />
-  </div>
+        <div className="col-md-4">
+          <div className="page-reveal reveal-quote">
+            <QuotePanel />
+          </div>
 
-  <div className="page-reveal reveal-book">
-    <BooksPanel />
-  </div>
-</div>
+          <div className="page-reveal reveal-book">
+            <BooksPanel />
+          </div>
+        </div>
 
-<div className="col-md-8 page-reveal reveal-carousel">
-  <InspirationalCarousel />
-</div>
+        <div className="col-md-8 page-reveal reveal-carousel">
+          <InspirationalCarousel />
+        </div>
       </div>
 
       <div className="decorative-frames">
@@ -40,6 +58,7 @@ function Home() {
           />
         </div>
       </div>
+
     </main>
   );
 }
